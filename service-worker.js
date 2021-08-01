a239d2b331e2f8b8ecf3a5d788684168bf617c41
nitinvemuri <80983324+nitinvemuri@users.noreply.github.com>
2021-08-01 03:19:28 -0700

    Deploying

--- service-worker.js
+++ service-worker.js
@@ -1,80 +1,62 @@
-const version = "version_01",
-const CACHE_NAME = "static-cache-v1";
-const DATA_CACHE_NAME = "data-cache-v1"
+const APP_PREFIX = 'BudgetTracker-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
 const FILES_TO_CACHE = [
-    "./index.html",
-    "/",
-    "/css/styles.css",
-    "/js/db.js",
-    "/js/index.js",
-    ".public/icons/icon-72x72.png",
-    "./public/icons/icon-96x96.png",
-    "./public/icons/icon-128x128.png",
-    "./public/icons/icon-144x144.png",
-    "./public/icons/icon-152x152.png",
-    "./public/icons/icon-192x192.png",
-    "./public/icons/icon-384x384.png",
-    "./public/icons/icon-512x512.png",
-    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
+    './index.html',
+    './css/styles.css',
+    './js/index.js',
+    './js/idb.js',
+    './manifest.json',
+    './icons/icon-512x512.png',
+    './icons/icon-384x384.png',
+    './icons/icon-192x192.png',
+    './icons/icon-152x152.png',
+    './icons/icon-144x144.png',
+    './icons/icon-128x128.png',
+    './icons/icon-96x96.png',
+    './icons/icon-72x72.png'
 ];
 
-self.addEventListener("install", (e) => {
+self.addEventListener('install', function (e) {
     e.waitUntil(
-      caches.open(CACHE_NAME).then((cache) => {
-        return cache.addAll(FILES_TO_CACHE);
-      })
-    );
-  
-    self.skipWaiting();
-  });
-  
-  self.addEventListener("activate", (e) => {
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME);
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+});
+
+self.addEventListener('activate', function (e) {
     e.waitUntil(
-      caches.keys().then((keyList) => {
-        return Promise.all(
-          keyList.map((key) => {
-            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-              return caches.delete(key);
-            }
-          })
-        );
-      })
-    );
-  
-    self.clients.claim();
-  });
-  
-  self.addEventListener("fetch", (evt) => {
-    if (e.request.url.includes("/api/") && e.request.method === "GET") {
-      e.respondWith(
-        caches
-          .open(DATA_CACHE_NAME)
-          .then((cache) => {
-            return fetch(e.request)
-              .then((response) => {
-                if (response.status === 200) {
-                  cache.put(e.request, response.clone());
+        caches.keys().then(function (keyList) {
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeeplist.push(CACHE_NAME);
+
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheKeeplist.indexOf(key) === -1) {
+                    console.log('deleting cache : ' + keyList[i] );
+                    return caches.delete(keyList[i]);
                 }
-  
-                return response;
-              })
-              .catch(() => {
-               
-                return cache.match(e.request);
-              });
-          })
-          .catch((err) => console.log(err))
-      );
-  
-     
-      return;
-    }
-  
-    
+            }));
+        })
+    )
+});
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url);
     e.respondWith(
-      caches.match(e.request).then((response) => {
-        return response || fetch(e.request);
-      })
-    );
-  });
+        caches.match(e.request).then(function (request) {
+            if (request) { // if cache is available, respond with cache
+                console.log('responding with cache : ' + e.request.url);
+                return request
+            } else {       // if there are no cache, try fetching request
+                console.log('file is not cached, fetching : ' + e.request.url);
+                return fetch(e.request)
+            }
+
+        })
+    )
+});
